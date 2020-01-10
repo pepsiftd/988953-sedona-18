b8395634c12f4b5a5174f200572237341802bfef
pepsiftd <pepsiftd@gmail.com>
2020-01-10 23:05:57 +0300

    Добавляет в сборку минимизацию html

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require("gulp-html-minifier");
 
 gulp.task("clean", function() {
   return del("build");
@@ -37,6 +38,7 @@ gulp.task("html", function() {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("build"))
 })
 
